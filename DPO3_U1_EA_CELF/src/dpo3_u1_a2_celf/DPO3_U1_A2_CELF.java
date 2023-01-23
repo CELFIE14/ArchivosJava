d9cd5fc8432685f377ac673b4d9193edd28e9430
CELFIE14 <osiriscelf4@hotmail.com>
2023-01-23 00:56:39 -0600

    Creación y visualización de archivos en Java

---
+++ DPO3_U1_EA_CELF/src/dpo3_u1_a2_celf/DPO3_U1_A2_CELF.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dpo3_u1_a2_celf;
+
+/**
+ *
+ * @author osiri
+ */
+public class DPO3_U1_A2_CELF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Nomina nom = new Nomina();//permite arrancar el metodo nomina
+        nom.setVisible(true);
+    }
+    
+}
